ce860d910670d2648f71fd8048bd760ae43e113d
Gabriel Sena <gabesenacom@gmail.com>
2021-09-27 09:46:57 -0300

    Define the product object instead of the product name
    If you want to expand later, you now have a lot of information about the product, instead just the name

--- src/components/CartNotifier.js
+++ src/components/CartNotifier.js
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 import { IconX } from '@tabler/icons'
 
-const CartNotifier = ({ productName, clearLastProduct }) => {
-  if (!productName) return null
+const CartNotifier = ({ lastProduct, clearLastProduct }) => {
+  if (!lastProduct) return null
   return (
     <div className='cart-notifier'>
       <div>
-        <p>Product {productName} added on your cart.</p>
+        <p>Product {lastProduct.name} added on your cart.</p>
         <Link to='/shopping-cart'>
           <p>Click here to check.</p>
         </Link>
