26ca713e8684c00863e85afbc84bd7733da8bcc5
Gabriel Sena <gabesenacom@gmail.com>
2021-09-26 23:15:16 -0300

    Remove random array on Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom'
-import { getRandomArray } from './../utils.js'
 import Banner from './../assets/banner.png'
 import ProductList from './../components/ProductList'
 
-const Home = ({ products }) => {
-  products = getRandomArray(products).slice(0, 4)
+const Home = ({ products, appendProductQuantity, productAddedNotifier }) => {
+  products = products.slice(0, 4)
   return (
     <section className='home-section'>
       <Link to='/shop'>
         <img src={Banner} alt='Banner' />
       </Link>
       <h3>Some Paintings</h3>
-      <ProductList products={products} />
+      <ProductList
+        products={products}
+        appendProductQuantity={appendProductQuantity}
+        productAddedNotifier={productAddedNotifier}
+      />
       <Link to='/shop' className='push-link'>
         click here to check all paints
       </Link>
