99311aa3f07fd0d0ec0fe310d6813dc244e3af73
Gabriel Sena <gabesenacom@gmail.com>
2021-09-26 21:20:05 -0300

    Add Support page

---
+++ src/pages/Support.js
@@ -0,0 +1,11 @@
+const Support = () => {
+  return (
+    <section>
+      <h3>Support</h3>
+      <p>Send us an email to help you.</p>
+      <p>Email: paintsshop@example.com</p>
+    </section>
+  )
+}
+
+export default Support
