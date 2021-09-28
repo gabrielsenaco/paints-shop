b0d510d86163fbb19e8cb12b0c246a2a00404143
Gabriel Sena <gabesenacom@gmail.com>
2021-09-28 10:50:34 -0300

    Add CartItemLabel component

---
+++ src/components/CartItemLabel.js
@@ -0,0 +1,13 @@
+const CartItemLabel = () => {
+  return (
+    <div className='cart-item-label'>
+      <p className='cart-item-label-image'>Image</p>
+      <p className='cart-item-label-name'>Name</p>
+      <p className='cart-item-label-price'>Price</p>
+      <p className='cart-item-label-quantity'>Quantity</p>
+      <p className='cart-item-label-actions'>Actions</p>
+    </div>
+  )
+}
+
+export default CartItemLabel
