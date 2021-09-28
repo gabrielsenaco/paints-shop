f81404e12e733974404143994fcc4e245b65c086
Gabriel Sena <gabesenacom@gmail.com>
2021-09-28 10:37:07 -0300

    Add cart notifier styles and restructure HTML message

--- src/components/CartNotifier.js
+++ src/components/CartNotifier.js
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom'
 import { IconX } from '@tabler/icons'
+import './../styles/CartNotifier.css'
 
 const CartNotifier = ({ lastProduct, clearLastProduct }) => {
   if (!lastProduct) return null
   return (
     <div className='cart-notifier'>
       <div>
-        <p>Product {lastProduct.name} added on your cart.</p>
-        <Link to='/shopping-cart'>
-          <p>Click here to check.</p>
-        </Link>
+        <p>
+          Product {lastProduct.name} added on your cart.{' '}
+          <Link to='/shopping-cart' onClick={clearLastProduct}>
+            Click here to check.
+          </Link>
+        </p>
       </div>
-      <IconX onClick={clearLastProduct} />
+      <IconX onClick={clearLastProduct} className='close-notification' />
     </div>
   )
 }
