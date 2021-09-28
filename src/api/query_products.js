6bcd01a1638ed691a4a64b9be4c26fb56ec7178a
Gabriel Sena <gabesenacom@gmail.com>
2021-09-28 12:54:44 -0300

    Remove testing api call

--- src/api/query_products.js
+++ src/api/query_products.js
@@ -6,7 +6,3 @@ export const fetchProducts = async url => {
   const json = await response.json()
   return json
 }
-
-fetchProducts(
-  'https://res.cloudinary.com/gabes/raw/upload/v1632837746/paints_t2fg3h.json'
-)
