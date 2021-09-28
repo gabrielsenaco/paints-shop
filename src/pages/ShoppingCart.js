2bd8a52a97231e3b0d476973c78431701e53540b
Gabriel Sena <gabesenacom@gmail.com>
2021-09-28 10:50:14 -0300

    Import ShoppingCart styles; Import useScrollTo custom hook

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -2,6 +2,9 @@ import { IconTrash, IconCheck } from '@tabler/icons'
 import { useHistory } from 'react-router-dom'
 import Button from './../components/Button'
 import CartItem from './../components/CartItem'
+import CartItemLabel from './../components/CartItemLabel'
+import { useScrollTop } from './../utils'
+import './../styles/ShoppingCart.css'
 
 const ShoppingCart = ({
   cartItems,
@@ -10,6 +13,8 @@ const ShoppingCart = ({
   reduceProductQuantity,
   removeProduct
 }) => {
+  useScrollTop()
+
   const history = useHistory()
 
   const clearCartHandler = () => {
@@ -52,15 +57,25 @@ const ShoppingCart = ({
     <section className='shopping-cart-section'>
       <form onSubmit={onConfirmPayment}>
         <h3>Shopping Cart</h3>
-        <div>
-          <p>{`${productsTotalQuantity} products. Total: ${productsTotalPrice}`}</p>
-          <Button type='button' onClick={clearCartHandler}>
+        <div className='cart-summary-container'>
+          <p className='cart-summary'>{`${productsTotalQuantity} ${
+            productsTotalQuantity > 1 ? 'products' : 'product'
+          }. Total: $ ${productsTotalPrice}`}</p>
+          <Button
+            type='button'
+            onClick={clearCartHandler}
+            className='clear-cart-btn'
+          >
             <IconTrash /> Clear Cart
           </Button>
         </div>
-        <ul className='shipping-cart-items'>{cartItems}</ul>
 
-        <Button type='submit'>
+        <ul className='shipping-cart-items'>
+          {cartItems.length > 0 && <CartItemLabel />}
+          {cartItems}
+        </ul>
+
+        <Button type='submit' className='confirm-payment-button'>
           <IconCheck /> Confirm Payment
         </Button>
       </form>
