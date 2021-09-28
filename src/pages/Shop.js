5c4a07a8e2fbeeafd6136d9a3e0a63452e53c3f9
Gabriel Sena <gabesenacom@gmail.com>
2021-09-28 10:49:57 -0300

    Import Shop styles; Import useScrollTo custom hook

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,6 +1,10 @@
 import ProductList from './../components/ProductList'
+import { useScrollTop } from './../utils'
+import './../styles/Shop.css'
 
 const Shop = ({ products, appendProductQuantity, productAddedNotifier }) => {
+  useScrollTop()
+
   return (
     <section className='shop-section'>
       <h3>Our paintings</h3>
