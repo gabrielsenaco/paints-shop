a813fd3e178f8eb41e3bd03878d1ff5ae81fa0c4
Gabriel Sena <gabesenacom@gmail.com>
2021-09-28 10:48:43 -0300

    Import start styles and sections; Import useScrollTo custom hook

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 import { Link } from 'react-router-dom'
 import Banner from './../assets/banner.png'
 import ProductList from './../components/ProductList'
+import { useScrollTop } from './../utils'
+import './../styles/sections.css'
+import './../styles/Home.css'
 
 const Home = ({ products, appendProductQuantity, productAddedNotifier }) => {
+  useScrollTop()
+
   products = products.slice(0, 4)
   return (
     <section className='home-section'>
-      <Link to='/shop'>
+      <Link to='/shop' className='banner'>
         <img src={Banner} alt='Banner' />
       </Link>
       <h3>Some Paintings</h3>
