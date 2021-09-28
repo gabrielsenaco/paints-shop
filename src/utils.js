4372f79034e596b561379c08995dcb0b7ccedfc9
Gabriel Sena <gabesenacom@gmail.com>
2021-09-28 10:48:29 -0300

    Add custom hook useScrollTop
    This hook will scroll the page to the top when the component is created.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
+import { useEffect } from 'react'
+
 export function getRandomArray (array) {
   return array.sort(() => 0.5 - Math.random())
+}
+
+export const useScrollTop = () => {
+  useEffect(() => {
+    window.scroll(0, 0)
+  }, [])
 }
