9c9c8ba778a6cc7169dc5ae50eed02a61bf1a092
Gabriel Sena <gabesenacom@gmail.com>
2021-09-28 10:42:26 -0300

    Restructure HTML Product tags and import all childrens

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,17 @@
 import { Link } from 'react-router-dom'
 
-const Product = ({ image, name, price, id }) => {
+const Product = ({ image, name, price, id, children }) => {
   return (
     <div className='product'>
       <Link to={`/shop/product/${id}`}>
         <img src={image} alt={name} />
       </Link>
-      <div>
-        <h4>{name}</h4>
-        <p>{price}</p>
+      <div className='product-infos'>
+        <div>
+          <h4>{name}</h4>
+          <p>$ {price}</p>
+        </div>
+        {children}
       </div>
     </div>
   )
